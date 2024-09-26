31ab33207e029f296b0a452f7587ea5c97f4cc2a
ViNx3h <vinhphuochongnnn@gmail.com>
2024-09-26 16:23:15 +0700

    layout preperation

--- src/Components/MobileNavigation.tsx
+++ src/Components/MobileNavigation.tsx
@@ -1,8 +1,15 @@
+import { BiSolidMoviePlay } from "react-icons/bi"
+import { MdHomeFilled } from "react-icons/md"
+import { PiTelevisionFill } from "react-icons/pi"
 
 const MobileNavigation = () => {
     return (
-        <section className="lg:hidden h-16 bg-neutral-600 bg-opacity-40 fixed bottom-0 w-full">
-            <div>MobileNavigation</div>
+        <section className="lg:hidden h-16 bg-neutral-600 bg-opacity-40 fixed bottom-0 w-full text-neutral-300">
+            <div className="items-center flex justify-between h-full ">
+                <a href="/tv" className="px-3 flex h-full items-center flex-col justify-center hover:text-neutral-100"><PiTelevisionFill className="text-2xl" />TV Shows</a>
+                <a href="/movie" className="px-3 flex h-full items-center flex-col justify-center hover:text-neutral-100"><BiSolidMoviePlay className="text-2xl" />Movies</a>
+                <a href="/Home" className="px-3 flex h-full items-center flex-col justify-center hover:text-neutral-100"><MdHomeFilled className="text-2xl" />Home</a>
+            </div>
         </section>
     )
 }
